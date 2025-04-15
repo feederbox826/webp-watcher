aeac6ad6d6145aea90064310b5b88f88d81ca43a
feederbox826 <feederbox826@users.noreply.github.com>
2025-04-14 22:48:55 -0400

    add webm support

--- watch.sh
+++ watch.sh
@@ -1,32 +1,50 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Starting watch.sh"
 
 input_dir="$1"
 output_dir="$2"
-if [[ -z "$input_dir" || -z "$output_dir" ]]; then
+if [ -z "$input_dir" ] || [ -z "$output_dir" ]; then
     echo "Usage: $0 <input_dir> <output_dir>"
     exit 1
 fi
 for dir in "$input_dir" "$output_dir"; do
-    [[ ! -d "$dir" ]] && echo "Directory does not exist: $dir" && exit 1
+    [ ! -d "$dir" ] && echo "Directory does not exist: $dir" && exit 1
 done
 
-function convert() {
-    local input_file="$1"
-    local target="${output_dir}/${input_file##*/}"
-    # Skip non-webp files or if the target file exists
-    [[ "$input_file" != *.webp || -f "$target" ]] && return
-
-    # reencode to quality 80
-    echo "Converting $input_file to ${target}"
-    cwebp -af -quiet -q 80 "$input_file" -o "${target}"
-    echo "Converted $input_file to ${target}"
+convert() {
+    input_file="$1"
+    target="${output_dir}/${input_file}"
+    # target file exists
+    if [ -f "$target" ]; then
+        return
+    # convert webp
+    elif [ "${input_file##*.}" = "webp" ]; then
+        # reencode to quality 80
+        cwebp -af -quiet -q 80 "${input_file}" -o "${target##*/}"
+        echo "Converted $input_file to ${target}"
+    elif [ "${input_file##*.}" = "webm" ]; then
+        # ffmpeg reencode to crf 25, bv 600k
+        ffmpeg -i "${input_file}" \
+            -c:v libx264 \
+            -quality best \
+            -b:v 600k \
+            -crf 25 \
+            -c:v libvpx-vp9 \
+            -c:a copy \
+            "${target}"
+        echo "Converted $input_file to ${target}.webm"
+    fi
 }
 
 # convert all files in the input directory
+echo "Converting all webp files"
 for file in "$input_dir"/*.webp; do
-    [[ -e "$file" ]] && convert "$file"
+    [ -e "$file" ] && convert "$file"
+done
+echo "Converting all webm files"
+for file in "$input_dir"/*.webm; do
+    [ -e "$file" ] && convert "$file"
 done
 
 echo "Watching $input_dir"
