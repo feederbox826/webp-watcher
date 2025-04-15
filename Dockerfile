c437aea738797f9eb8799e125ae2f56e9d4ffe64
feederbox826 <feederbox826@users.noreply.github.com>
2025-04-15 02:22:47 -0400

    switch to posix shell

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM alpine
-RUN apk add --no-cache bash libwebp-tools inotify-tools
+RUN apk add --no-cache libwebp-tools inotify-tools
 COPY --chmod=555 watch.sh /watch.sh 
 CMD ["/watch.sh", "/input", "/output"]
 VOLUME ["/input", "/output"]
--- watch.sh
+++ watch.sh
@@ -1,36 +1,43 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Starting watch.sh"
 
-input_dir="$1"
-output_dir="$2"
-if [[ -z "$input_dir" || -z "$output_dir" ]]; then
+input_dir=$1
+output_dir=$2
+if [ -z "$input_dir" ] || [ -z "$output_dir" ]; then
     echo "Usage: $0 <input_dir> <output_dir>"
     exit 1
 fi
+
+# ensure directories exist
 for dir in "$input_dir" "$output_dir"; do
-    [[ ! -d "$dir" ]] && echo "Directory does not exist: $dir" && exit 1
+    if [ ! -d "$dir" ]; then
+        echo "Directory does not exist: $dir"
+        exit 1
+    fi
 done
 
-function convert() {
-    local input_file="$1"
-    local target="${output_dir}/${input_file##*/}"
+convert() {
+    input_file="$1"
+    base_name=${input_file##*/}
+    target=$output_dir/$base_name
     # Skip non-webp files or if the target file exists
-    [[ "$input_file" != *.webp || -f "$target" ]] && return
-
+    case "$input_file" in
+        *.webp) [ ! -f "$target" ] || return ;;
+        *) return ;;
+    esac
     # reencode to quality 80
     echo "Converting $input_file to ${target}"
-    cwebp -af -quiet -q 80 "$input_file" -o "${target}"
+    cwebp -af -quiet -q 80 "$input_file" -o "${target}" &&
     echo "Converted $input_file to ${target}"
 }
 
 # convert all files in the input directory
 for file in "$input_dir"/*.webp; do
-    [[ -e "$file" ]] && convert "$file"
+    [ -e "$file" ] && convert "$file"
 done
 
 echo "Watching $input_dir"
-    inotifywait -mrqe attrib "$input_dir" --format %w%f | while read -r file; do
-        convert "$file"
+inotifywait -mrqe attrib "$input_dir" --format %w%f | while IFS= read -r file; do
+    convert "$file"
 done
-echo "watch.sh finished"
