d42cebd9fe4fa6d69eea039fa5ec3f6750aa2022
feederbox826 <feederbox826@users.noreply.github.com>
2025-04-15 02:13:42 -0400

    Revert "add webm support"
    This reverts commit aeac6ad6d6145aea90064310b5b88f88d81ca43a.
    
    too much hassle for too much quality loss

--- watch.sh
+++ watch.sh
@@ -1,62 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Starting watch.sh"
 
 input_dir="$1"
 output_dir="$2"
-if [ -z "$input_dir" ] || [ -z "$output_dir" ]; then
+if [[ -z "$input_dir" || -z "$output_dir" ]]; then
     echo "Usage: $0 <input_dir> <output_dir>"
     exit 1
 fi
 for dir in "$input_dir" "$output_dir"; do
-    [ ! -d "$dir" ] && echo "Directory does not exist: $dir" && exit 1
+    [[ ! -d "$dir" ]] && echo "Directory does not exist: $dir" && exit 1
 done
 
-convert() {
-    input_file="$1"
-    target="${output_dir}/${input_file##*/}"
-    # target file exists
-    if [ -f "$target" ]; then
-        return
-    # convert webp
-    elif [ "${input_file##*.}" = "svg" ]; then
-        # copy to optimized
-        cp "${input_file}" "${target}.svg"
-    elif [ "${input_file##*.}" = "webp" ]; then
-        # reencode to quality 80
-        cwebp -af -quiet -q 80 "${input_file}" -o "${target}"
-        echo "Converted $input_file to ${target}"
-    elif [ "${input_file##*.}" = "webm" ]; then
-        # ffmpeg reencode to crf 25, bv 1M, 2 pass
-        ffmpeg -i "${input_file}" \
-            -c:v libvpx-vp9 \
-            -deadline good \
-            -b:v 1M \
-            -crf 25 \
-			-row-mt 1 \
-			-pass 1 \
-            -an \
-			-f null /dev/null && \
-		ffmpeg -i "${input_file}" \
-            -c:v libvpx-vp9 \
-            -deadline good \
-            -b:v 1M \
-            -crf 25 \
-			-row-mt 1 \
-			-pass 2 \
-            "${target}"
-        echo "Converted $input_file to ${target}.webm"
-    fi
+function convert() {
+    local input_file="$1"
+    local target="${output_dir}/${input_file##*/}"
+    # Skip non-webp files or if the target file exists
+    [[ "$input_file" != *.webp || -f "$target" ]] && return
+
+    # reencode to quality 80
+    echo "Converting $input_file to ${target}"
+    cwebp -af -quiet -q 80 "$input_file" -o "${target}"
+    echo "Converted $input_file to ${target}"
 }
 
 # convert all files in the input directory
-echo "Converting all webp files"
 for file in "$input_dir"/*.webp; do
-    [ -e "$file" ] && convert "$file"
-done
-echo "Converting all webm files"
-for file in "$input_dir"/*.webm; do
-    [ -e "$file" ] && convert "$file"
+    [[ -e "$file" ]] && convert "$file"
 done
 
 echo "Watching $input_dir"
